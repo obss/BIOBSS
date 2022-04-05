cc1a0f8f0db1113e7360311a483bb75a6da29c7f
Ipek Karakus <ipek.karakus@obss.com.tr>
2022-04-05 14:40:32 +0300

    added rqi methods

--- biobss/resptools/rr_estimation.py
+++ biobss/resptools/rr_estimation.py
@@ -1,13 +1,14 @@
 import numpy as np
 import scipy as sp
+import pandas as pd
 import neurokit2 as nk
 from numpy.typing import ArrayLike
 
 from ..signaltools import peakdetection
+from ..edatools import hjorth
 
 
-
-def extract_resp_sig(peaks_locs: ArrayLike, peaks_amp: ArrayLike, troughs_amp: ArrayLike, sampling_rate: float, mod_type: str='All',resampling_rate: float=10) -> dict:
+def extract_resp_sig(peaks_locs: ArrayLike, peaks_amp: ArrayLike, troughs_amp: ArrayLike, sampling_rate: float, mod_type: str=['AM','FM','BW'],resampling_rate: float=10) -> dict:
     """Calculates the modulations resulted from respiratory activity.
 
     Args:
@@ -15,134 +16,138 @@ def extract_resp_sig(peaks_locs: ArrayLike, peaks_amp: ArrayLike, troughs_amp: A
         peaks_amp (ArrayLike): PPG signal peak amplitudes
         troughs_amp (ArrayLike): PPG signal trough amplitudes
         sampling_rate (float): Sampling rate of the original PPG signal
-        mod_type (str, optional): Modulation type: 'AM' for amplitude modulation, 'FM' for frequency modulation, 'BW' for baseline wander. Defaults to 'All'.
+        mod_type (Array, optional): Modulation type: 'AM' for amplitude modulation, 'FM' for frequency modulation, 'BW' for baseline wander. Defaults to ['AM','FM','BW'].
         resampling_rate (float, optional): Sampling rate of the extracted respiratory signal. Defaults to 10.
 
     Returns:
         dict: Dictionary of extracted respiratory signals.
     """
 
-     
     info={}
     rs_len=int(sampling_rate/resampling_rate)
 
-    #Amplitude modulation
-    am=peaks_amp-troughs_amp[:-1]
-    am_x=peaks_locs
-    am_interp=sp.interpolate.interp1d(am_x,am)
-    am_num=peaks_locs[-1]-peaks_locs[0]+1
-    am_xnew=np.linspace(peaks_locs[0],peaks_locs[-1],am_num)
-    am_new=am_interp(am_xnew)
+    if 'AM' in mod_type:
+        #Amplitude modulation
+        am=peaks_amp-troughs_amp[:-1]
+        am_x=peaks_locs
+        am_interp=sp.interpolate.interp1d(am_x,am)
 
-    am_respsignal=am_new[::rs_len]
-    am_xrs=am_xnew[::rs_len]
-    am_xrs=am_xrs.astype(int)
+        am_num=peaks_locs[-1]-peaks_locs[0]+1
+        am_xnew=np.linspace(peaks_locs[0],peaks_locs[-1],am_num)
+        am_new=am_interp(am_xnew)
 
-    #Frequency modulation
-    fm=np.diff(peaks_locs)/sampling_rate
-    fm_x=peaks_locs[:-1]
-    fm_interp=sp.interpolate.interp1d(fm_x,fm)
-    fm_num=peaks_locs[-2]-peaks_locs[0]+1
-    fm_xnew=np.linspace(peaks_locs[0],peaks_locs[-2],fm_num)
-    fm_new=fm_interp(fm_xnew)
+        am_respsignal=am_new[::rs_len]
+        am_xrs=am_xnew[::rs_len].astype(int)
 
-    fm_respsignal=fm_new[::rs_len]
-    fm_xrs=fm_xnew[::rs_len]
-    fm_xrs=fm_xrs.astype(int)
+        info['am_y']=am_respsignal
+        info['am_x']=am_xrs
 
-    #Baseline wander
-    bw=(peaks_amp+troughs_amp[:-1])/2
-    bw_x=peaks_locs
-    bw_interp=sp.interpolate.interp1d(bw_x,bw)
-    bw_num=peaks_locs[-1]-peaks_locs[0]+1
-    bw_xnew=np.linspace(peaks_locs[0],peaks_locs[-1],bw_num)
-    bw_new=bw_interp(bw_xnew)
+    if 'FM' in mod_type:
 
-    bw_respsignal=bw_new[::rs_len]
-    bw_xrs=bw_xnew[::rs_len]
-    bw_xrs=bw_xrs.astype(int)
+        #Frequency modulation
+        fm=np.diff(peaks_locs)/sampling_rate
+        fm_x=peaks_locs[:-1]
+        fm_interp=sp.interpolate.interp1d(fm_x,fm)
 
-    if mod_type=='AM':
+        fm_num=peaks_locs[-2]-peaks_locs[0]+1
+        fm_xnew=np.linspace(peaks_locs[0],peaks_locs[-2],fm_num)
+        fm_new=fm_interp(fm_xnew)
 
-        info['Resp. signal']=am_respsignal
-        info['x']=am_xrs
+        fm_respsignal=fm_new[::rs_len]
+        fm_xrs=fm_xnew[::rs_len].astype(int)
 
-    elif mod_type=='FM':
+        info['fm_y']=fm_respsignal
+        info['fm_x']=fm_xrs
 
-        info['Resp. signal']=fm_respsignal
-        info['x']=fm_xrs
+    if 'BW' in mod_type:
 
-    elif mod_type=='BW':
+        #Baseline wander
+        bw=(peaks_amp+troughs_amp[:-1])/2
+        bw_x=peaks_locs
+        bw_interp=sp.interpolate.interp1d(bw_x,bw)
 
-        info['Resp. signal']=bw_respsignal
-        info['x']=bw_xrs
+        bw_num=peaks_locs[-1]-peaks_locs[0]+1
+        bw_xnew=np.linspace(peaks_locs[0],peaks_locs[-1],bw_num)
+        bw_new=bw_interp(bw_xnew)
 
-    else:
-        info['AM']=am_respsignal
-        info['Am_x']=am_xrs
-        info['FM']=fm_respsignal
-        info['FM_x']=fm_xrs
-        info['BW']=bw_respsignal
-        info['BW_x']=bw_xrs        
+        bw_respsignal=bw_new[::rs_len]
+        bw_xrs=bw_xnew[::rs_len].astype(int)
+
+        info['bw_y']=bw_respsignal
+        info['bw_x']=bw_xrs
+      
 
     return info
 
 
 
 
-def filter_resp(y_am: ArrayLike=None, y_fm: ArrayLike=None, y_bw: ArrayLike=None, resampling_rate: float=10) -> dict:
+def filter_resp_sig(resampling_rate: float=10, **kwargs) -> dict:
     """Filters extracted respiratory signal
 
     Args:
-        y_am (Array, optional):Respiratory signal calculated from amplitude modulation. Defaults to None.
-        y_fm (Array, optional):Respiratory signal calculated from frequency modulation. Defaults to None.
-        y_bw (Array, optional):Respiratory signal calculated from baseline wander. Defaults to None.
         resampling_rate (float, optional): Resampling rate. Defaults to 10.
 
+    kwargs:
+        am_sig (Array, optional):Respiratory signal calculated from amplitude modulation. Defaults to None.
+        am_x (Array, optional):x-axis values of amplitude modulation signal. Defaults to None.
+
+        fm_sig (Array, optional):Respiratory signal calculated from frequency modulation. Defaults to None.
+        fm_x (Array, optional):x-axis values of frequency modulation signal. Defaults to None.
+
+        bw_sig (Array, optional):Respiratory signal calculated from baseline wander. Defaults to None.
+        bw_x (Array, optional):x-axis values of baseline wander signal. Defaults to None.
+
+
     Returns:
-        Dict: Dictionary of filtered respiratory signals.
+        dict: Dictionary of filtered respiratory signals.
     """
+
 
     filtered_resp={}
 
-    if y_am is not None:
-        cl_am=nk.rsp_clean(y_am,sampling_rate=resampling_rate,mod_type='khodadad2018')
-        filtered_resp['AM']=cl_am
+    if 'am_sig' in kwargs.keys():
+        cl_am=nk.rsp_clean(kwargs['am_sig'],sampling_rate=resampling_rate,method='khodadad2018')
+        filtered_resp['am_sig']=cl_am
+        filtered_resp['am_x']=kwargs['am_x']
 
-    if y_fm is not None:
-        cl_fm=nk.rsp_clean(y_fm,sampling_rate=resampling_rate,mod_type='khodadad2018')
-        filtered_resp['FM']=cl_fm
+    if 'fm_sig' in kwargs.keys():
+        cl_fm=nk.rsp_clean(kwargs['fm_sig'],sampling_rate=resampling_rate,method='khodadad2018')
+        filtered_resp['fm_sig']=cl_fm
+        filtered_resp['fm_x']=kwargs['fm_x']
 
-    if y_bw is not None:
-        cl_bw=nk.rsp_clean(y_bw,sampling_rate=resampling_rate,mod_type='khodadad2018')    
-        filtered_resp['BW']=cl_bw
+    if 'bw_sig' in kwargs.keys():
+        cl_bw=nk.rsp_clean(kwargs['bw_sig'],sampling_rate=resampling_rate,method='khodadad2018')    
+        filtered_resp['bw_sig']=cl_bw
+        filtered_resp['bw_x']=kwargs['bw_x']
     
 
     return filtered_resp
 
 
 
-def estimate_rr(resp_sig: ArrayLike, sampling_rate: float, mod_type: str='peakdet',delta: float=0.001) -> float:
+def estimate_rr(resp_sig: ArrayLike, sampling_rate: float, method: str='peakdet',delta: float=0.001) -> float:
     """Estimates respiratory rate from the respiratory signal.
 
     Args:
         resp_sig (Array): Extracted respiratory signal.
         sampling_rate (float): Sampling rate (resampled)
-     mod_type (str): mod_type of rr estimation. 'peakdet', 'count_adv' or 'xcorr'. Defaults to 'peakdet'.
+        method (str): mod_type of rr estimation. 'peakdet', 'count_adv' or 'xcorr'. Defaults to 'peakdet'.
         delta (float, optional): Parameter of 'peakdet' mod_type. Defaults to 0.001.
 
     Returns:
         float: Estimated respiratory rate
     """
     
-    if  (mod_type=='peakdet'):
+    if  (method=='peakdet'):
         
         #peakdet
         maxtab,mintab=peakdetection._peakdetection_peakdet(resp_sig,delta)
 
         if (maxtab.size==0) or (mintab.size==0):
-            print("Respiratory rate can not be estimated!")
             mean_rr=[]
+            print("No peaks or troughs found.Respiratory rate can not be estimated!")
+            
 
         else:
 
@@ -156,7 +161,7 @@ def estimate_rr(resp_sig: ArrayLike, sampling_rate: float, mod_type: str='peakde
             mean_rr=60/mean_int #br/minutes
 
        
-    elif  (mod_type=='count_adv'):
+    elif  (method=='count_adv'):
     
         peaks_locs,_=sp.signal.find_peaks(resp_sig)
         peaks=resp_sig[peaks_locs]
@@ -165,8 +170,9 @@ def estimate_rr(resp_sig: ArrayLike, sampling_rate: float, mod_type: str='peakde
         #troughs=resp_sig[locs_troughs]
 
         if (peaks_locs.size==0) or (locs_troughs.size==0):
-            print("Respiratory rate can not be estimated!")
             mean_rr=[]
+            print("No peaks or troughs found.Respiratory rate can not be estimated!")
+            
         
         else:
     
@@ -199,7 +205,7 @@ def estimate_rr(resp_sig: ArrayLike, sampling_rate: float, mod_type: str='peakde
             mean_int=np.mean(intervals)
             mean_rr=60/mean_int #br/minutes
     
-    elif  (mod_type=='xcorr'):
+    elif  (method=='xcorr'):
         
         rr_inst = nk.rsp_rate(resp_sig, sampling_rate=sampling_rate, mod_type="xcorr")
         
@@ -207,14 +213,48 @@ def estimate_rr(resp_sig: ArrayLike, sampling_rate: float, mod_type: str='peakde
           
         
     else:
-        print("The mod_type should be 'peakdet','count_adv' or 'xcorr'")
+        raise ValueError("The method should be 'peakdet','count_adv' or 'xcorr'")
         
 
     
     return mean_rr
 
+def calc_rqi(resp_sig, f_rs=10, rqi_method=['autocorr','hjorth']) -> dict:
 
-def fuse_rr(rr_am: float=None, rr_fm: float=None, rr_bw: float=None, fusion_method: str='Smart') -> float:
+    rqindices={}
+
+    if 'autocorr' in rqi_method:
+
+        corr_coeff=[]
+
+        sig=pd.Series(resp_sig)
+        lag_min=int(1.33*f_rs) #samples
+        lag_max=int(5*f_rs) #samples #should be 10
+
+        
+        for lag in range(lag_min,lag_max+1):
+
+            c=sig.autocorr(lag=lag)
+
+            corr_coeff.append(c)
+
+        
+        rqindices['autocorr']=max(corr_coeff)
+
+
+    if 'hjorth' in rqi_method:
+
+        hjorth_par=hjorth.get_hjorth_features(resp_sig)
+
+        rqindices['hjorth']=hjorth_par['signal_complexity']
+        
+    
+
+
+    return rqindices
+
+
+def fuse_rr(fusion_method: str='SmartFusion', rqi=None, **kwargs) -> float:
     """Fuses respiratory rates calculated from different modulation types.
 
     Args:
@@ -226,25 +266,44 @@ def fuse_rr(rr_am: float=None, rr_fm: float=None, rr_bw: float=None, fusion_meth
     Returns:
         _type_: _description_
     """
-    if rr_am is None:
-        rr_am=[]
-    if rr_fm is None:
-        rr_fm=[]
-    if rr_bw is None:
-        rr_bw=[]
+
+    if kwargs:
+
+        rr_est=[]
+        
+        for mod_type in kwargs.keys():
+            rr_est.append(kwargs[mod_type])
+
+        
+
+        if fusion_method=='SmartFusion': #Smart Fusion
+                       
+            rr_std=np.std(rr_est)
+
+            if (rr_std <= 4 ):
+                rr_fused=np.mean(rr_est)               
+            else:  
+                rr_fused=[]
 
 
-    if fusion_method=='Smart':
-        #Smart Fusion
-        rr_est=np.array([rr_am,rr_fm,rr_bw])
-        rr_std=np.std(rr_est)
+        elif fusion_method=='QualityFusion':
 
-        if (rr_std <= 4 ):
-            rr_fused=np.mean(rr_est)            
-            
+            if rqi is not None:
+
+                rr_fused= np.sum(np.asarray(rqi)*np.asarray(rr_est))/np.sum(rqi)
+
+            else:
+
+                raise ValueError("RQI values are required.'")
+
         else:
-            
-            rr_fused=[]
+            raise ValueError("The method should be 'SmartFusion' or 'QualityFusion'.") 
+
+
+
+    else:
+        raise ValueError("At least one respiratory rate should be given.'")
+
 
     return rr_fused
 
--- biobss/resptools/rr_preprocess.py
+++ biobss/resptools/rr_preprocess.py
@@ -1,7 +1,7 @@
 from ..signaltools import filtering
 from numpy.typing import ArrayLike
 
-def elim_vlf(sig: ArrayLike, sampling_rate: float) -> ArrayLike:
+def elim_vlf(ppg_sig: ArrayLike, sampling_rate: float) -> ArrayLike:
     """Eliminates very low frequencies prior to respiratory rate estimation procedure. 
     The cutoff frequencies are determined considering the frequency range of the respiration.
 
@@ -16,12 +16,12 @@ def elim_vlf(sig: ArrayLike, sampling_rate: float) -> ArrayLike:
     filter_type='highpass'
     f1=0.0665 #4 bpm
     
-    filtered_signal=filtering.filter_signal(sig,filter_type,N,sampling_rate,f1,[])
+    filtered_signal=filtering.filter_signal(ppg_sig,filter_type,N,sampling_rate,f1,[])
 
     return filtered_signal
 
 
-def elim_vhf(sig: ArrayLike, sampling_rate: float) -> ArrayLike:
+def elim_vhf(ppg_sig: ArrayLike, sampling_rate: float) -> ArrayLike:
     """Eliminates very high frequencies prior to respiratory rate estimation procedure. 
     The cutoff frequencies are determined considering the frequency range of the respiration.
 
@@ -36,7 +36,7 @@ def elim_vhf(sig: ArrayLike, sampling_rate: float) -> ArrayLike:
     filter_type='lowpass'
     f2=30 
     
-    filtered_signal=filtering.filter_signal(sig,filter_type,N,sampling_rate,[],f2)
+    filtered_signal=filtering.filter_signal(ppg_sig,filter_type,N,sampling_rate,[],f2)
     
     return filtered_signal
 
