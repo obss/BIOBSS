f69ed73c9d707f6b61a90363f594fc39050133b8
Çağatay Taşcı <cagatay.tasci@obss.com.tr>
2022-04-13 14:03:00 +0300

    Structure commit.

---
+++ biobss/pipeline/process.py
@@ -0,0 +1,31 @@
+"""generic signal process object"""
+
+class Bio_process():
+    
+    def __init__(self,method,modality,**kwargs) -> None:
+        self.method=method
+        self.modality=modality
+        self.kwargs=kwargs
+        
+        
+    def check_modality(self,modality):
+        if(self.modality==modality):
+            return True
+        else:
+            return False
+        
+    def check_sigtype(self,sigtype):
+        if(self.sigtype==sigtype):
+            return True
+        else:
+            return False
+        
+    def process(self,signal,sampling_rate):    
+        return 
+    
+    def get_name(self):
+        return self.method
+
+    
+    
+
---
+++ biobss/pipeline/process_list.py
@@ -0,0 +1,22 @@
+
+""" Process list object with add and iterate process objects"""
+class Process_list():
+    """ Process list object with add and iterate process objects"""
+    def __init__(self,modality,sigtype):
+        self.process_list=[]
+        self.modality=modality
+        self.sigtype=sigtype
+        
+    def add_process(self,process):
+        if(not process.check_modality(self.modality)):
+            raise ValueError('Modality of process does not match modality of process list')
+        elif(not process.check_sigtype(self.sigtype)):
+            raise ValueError('Signal type of process does not match signal type of process list')
+        else:
+            self.process_list.append(process)
+            
+    def get_process_by_name(self,name):
+        for process in self.process_list:
+            if(process.name==name):
+                return process
+        raise ValueError('Process with name '+name+' not found')
