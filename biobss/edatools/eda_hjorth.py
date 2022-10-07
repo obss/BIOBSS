1b3653fdd3b535b4d621c90241f689596d372441
Ipek Karakus <ipek.karakus@obss.com.tr>
2022-10-07 17:20:49 +0300

    edatools reorganization

---
+++ biobss/edatools/eda_hjorth.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+import numpy as np
+from numpy.typing import ArrayLike
+
+
+def get_hjorth_features(signal: ArrayLike, prefix="signal"):
+    """This method returns Hjörth parameters for the given signal.
+    For more details, see the https://en.wikipedia.org/wiki/Hjorth_parameters
+
+    Args:
+        signal (ArrayLike): Input signal
+        prefix (str, optional): prefix for signal name. Defaults to "signal".
+
+    Returns:
+        dict: calculated hjorth parameters
+    """
+
+    h_features = {}
+    h_features[prefix + "_activity"] = calculate_activity(signal)
+    h_features[prefix + "_complexity"], h_features[prefix + "_mobility"] = calculate_complexity(
+        signal
+    )
+    return h_features
