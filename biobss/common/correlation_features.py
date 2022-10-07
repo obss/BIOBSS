97e6239b1fbecdf0f78dba19d7e82345e1e700df
Ipek Karakus <ipek.karakus@obss.com.tr>
2022-10-07 17:10:52 +0300

    Revert "reorganized common functions"
    This reverts commit b6a81b1803f13a400390d7de0c7f5c9ddf853ee4.

---
+++ biobss/common/correlation_features.py
@@ -0,0 +1,23 @@
+from scipy.stats import pearsonr
+import itertools
+from numpy.typing import ArrayLike
+
+
+def correlation_features(signals: ArrayLike, signal_names: ArrayLike, prefix="signal", **kwargs) -> dict:
+    """a function to calculate correlation of N signals by each other
+
+    Args:
+        signals (2-D Arraylike): Array of signals
+        signal_names (Arraylike): Array of signal names
+
+    Returns:
+        dict : dict of correlation features
+    """
+
+    data = dict(zip(signal_names, signals))
+    comb = list(itertools.combinations(signal_names, 2))
+    corr_list = {}
+    for i in comb:
+        corr_list["".join(i) + "_correl"] = pearsonr(data[i[0]], data[i[1]])[0]
+
+    return corr_list
--- biobss/ppgtools/ppg_statistical.py
+++ biobss/ppgtools/ppg_statistical.py
@@ -1,8 +1,7 @@
 import numpy as np
 from scipy import stats
+import collections
 from numpy.typing import ArrayLike
-
-from biobss.common.signal_entropy import *
 
 #Statistical features
 FEATURES_STAT_CYCLE = {
@@ -71,3 +70,22 @@ def get_stat_features(sig: ArrayLike, sampling_rate: float,type: str, prefix: st
     return features_stat
 
 
+def calculate_shannon_entropy(sig: ArrayLike, base: int=2) -> float:
+    """Calculates shannon entropy of the signal.
+
+    Args:
+        sig (ArrayLike): Signal to be analyzed.
+        base (int): The logarithmic base to use, defaults to 2.
+
+    Returns:
+        float: Shannon entropy of the signal.
+    """
+
+    bases = collections.Counter([tmp_base for tmp_base in sig])
+    # define distribution
+    dist = [x/sum(bases.values()) for x in bases.values()]
+ 
+    # use scipy to calculate entropy
+    entropy_value = stats.entropy(dist, base=base)
+ 
+    return entropy_value
